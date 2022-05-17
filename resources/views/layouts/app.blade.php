61bb2ddc9a52f50f7abf8b4ecdd044017071e6e1
vankatwijk <hendrikus.hpvk@gmail.com>
2022-05-17 10:27:17 +0200

    meta data added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,9 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>HPVK.com | the Swiss army knife of tiny web tools</title>
+    <meta name="description" content="HPVK.com the Swiss army knife of tiny web tools, Free URL shortener shortens your unwieldly links into more manageable and useable URLs">
+    <meta name="keywords" content="hpvk tinyurl url save share shorten analyze web tools">
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -19,6 +21,7 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&display=swap" rel="stylesheet">
 </head>
 <body class="bg-gray-200 font-sans">
+    <div style="display:none;">HPVK.com the Swiss army knife of tiny web tools, Free URL shortener shortens your unwieldly links into more manageable and useable URLs</div>
 <div id="app">
     @guest()
         <navigation-bar :user="''"></navigation-bar>
