47fdb573608c531c42c816544d2675fe5b18d497
vankatwijk <hendrikus.hpvk@gmail.com>
2022-05-18 10:49:29 +0200

    cleanup

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,10 +24,12 @@
 
 <div style="display:none;">HPVK.com the Swiss army knife of tiny web tools, Free URL shortener shortens your unwieldly links into more manageable and useable URLs</div>
 <div id="app">
+    
     <video autoplay="" loop="" muted="" poster="https://www.pexels.com/assets/videos/free-videos-7daa2ef41a140f70c757ce91913a4ecb90570b7d7cd2b401bae868350e02c83a.jpg">
         <source src="https://static.pexels.com/lib/videos/free-videos.mp4" type="video/mp4">
         <source src="https://static.pexels.com/lib/videos/free-videos.webm" type="video/webm">
     </video>
+
     @guest()
         <navigation-bar :user="''"></navigation-bar>
     @else
