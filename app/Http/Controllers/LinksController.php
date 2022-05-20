5759d1808c2344cf93375c59365c03c3e5e5d8c9
vankatwijk <hendrikus.hpvk@gmail.com>
2022-05-20 16:16:50 +0200

    backend file upload

--- app/Http/Controllers/LinksController.php
+++ app/Http/Controllers/LinksController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\ShowShortLink;
-use App\Http\Requests\StoreShortLink;
 use App\Link;
 use Carbon\Carbon;
+use App\Http\Requests\ShowShortLink;
+use App\Http\Requests\StoreShortLink;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Requests\StoreShortLinkUpload;
 use App\Http\Resources\Link as LinkResource;
 
 class LinksController extends Controller
@@ -31,13 +33,34 @@ class LinksController extends Controller
         return response()->json(new LinkResource($link));
     }
 
+    public function storeupload(StoreShortLinkUpload $request)
+    {
+
+        $data = $request->validated();
+
+        $link = $this->createUploadForUserOrGuest($data);
+
+        return response()->json(new LinkResource($link));
+    }
+
     public function show(ShowShortLink $request, Link $link)
     {
         $data = $request->validated();
 
         $link->addClick(Carbon::now());
 
-        return redirect()->to($link->original);
+        if($link->original == '@file'){
+
+            if(Storage::disk('public')->assertExists('files/'.$link->file.'/'.$link->file_name)){
+                return Storage::disk('public')->download('files/'.$link->file.'/'.$link->file_name);
+            }else{
+                return view('links.create');
+            }
+        }else{
+
+            return redirect()->to($link->original);
+        }
+
     }
 
     private function getLinksForUserOrGuest()
@@ -56,5 +79,25 @@ class LinksController extends Controller
         }
 
         return auth()->user()->links()->create($data);
+    }
+
+    private function createUploadForUserOrGuest($data)
+    {
+        $uploadedFile = $data['file'];
+        $folderName = time();
+
+        //set the folder name to the current time and store the file within it
+        Storage::disk('public')->putFileAs('files/'.$folderName,$uploadedFile,$uploadedFile->getClientOriginalName());
+
+        //file information to store in the db
+        $finaldata['original'] = $data['original'];//placeholder with '@file' to mae a difference between files and urls
+        $finaldata['file'] = $folderName; //folder name where the files are stored
+        $finaldata['file_name'] = $uploadedFile->getClientOriginalName();//original name of the file - $uploadedFile->getClientOriginalExtension();
+
+        if (auth()->guest()) {
+            return Link::create($finaldata);
+        }
+
+        return auth()->user()->links()->create($finaldata);
     }
 }
